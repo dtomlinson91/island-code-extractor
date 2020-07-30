8268b567085f6a03ea96c459d9a96c9f85373e7c
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2020-07-30 18:40:33 +0100

    Panaetius structure

---
+++ src/island_code_extractor/__init__.py
@@ -0,0 +1,8 @@
+"""Island code extractor for Animal Crossing."""
+
+import panaetius
+
+panaetius.set_config(panaetius.CONFIG, "reddit.secret")
+panaetius.set_config(panaetius.CONFIG, "reddit.id")
+panaetius.set_config(panaetius.CONFIG, "reddit.user_agent")
+panaetius.set_config(panaetius.CONFIG, "reddit.thread_id")
