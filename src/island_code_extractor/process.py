8a3aa3fa9ca44e89accd2516b9734e2481c2a838
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2020-08-05 01:30:43 +0100

    Adding pull comments by thread

--- src/island_code_extractor/process.py
+++ src/island_code_extractor/process.py
@@ -1,40 +1,60 @@
+from typing import List, Dict
+import re
+
 from praw.models import Comment
 
-import island_code_extractor
-from island_code_extractor import reddit
+
+def _extract_time(comment_instance: Comment) -> str:
+    return comment_instance.created_utc
 
 
-def _extract_time(comment: Comment) -> str:
-    return comment.created_utc
+def _extract_body(comment_instance: Comment) -> str:
+    return comment_instance.body
 
 
-def _extract_body(comment: Comment) -> str:
-    return comment.body
+def _extract_user(comment_instance: Comment) -> str:
+    return comment_instance.author.name
 
 
-def _extract_user(comment: Comment) -> str:
-    return comment.author
+def _extract_permalink(comment_instance: Comment) -> str:
+    return f"https://reddit.com{comment_instance.permalink}"
 
 
-def _extract_permalink(comment: Comment) -> str:
-    return comment.permalink
+def _extract_dream_code(comment_instance: Comment) -> str:
+    dream_address_re = r"(DA(?:\-[0-9]{4}){3})"
+    if match := re.search(dream_address_re, comment_instance.body, re.IGNORECASE):
+        return match.group(1)
+    return False
 
 
-def _extract_code(comment: Comment) -> str:
+def _extract_country(comment_instance: Comment) -> str:
     pass
 
 
-def _extract_country(comment: Comment) -> str:
+def _extract_keywords(comment_instance: Comment) -> str:
     pass
 
 
-def _extract_keywords(comment: Comment) -> str:
-    pass
+def get_comment_data(praw_comments: List[Comment]) -> List[Dict]:
+    """
+    Return data from comments.
 
+    Args:
+        praw_comments (List[Comment]): A list containing praw `Comment` objects.
 
-if __name__ == "__main__":
-    reddit_instance = reddit.get_reddit_instance()
-    comments = reddit.get_hourly_comments(reddit_instance)
-    for comment in comments:
-        print(_extract_permalink(comment))
-
+    Returns:
+        List[Dict]: A list of the properties of a comment.
+    """
+    return [
+        dict(
+            [
+                ("time", _extract_time(comment)),
+                ("body", _extract_body(comment)),
+                ("user", _extract_user(comment)),
+                ("permalink", _extract_permalink(comment)),
+                ("dream_code", _extract_dream_code(comment)),
+            ]
+        )
+        for comment in praw_comments
+        if _extract_dream_code(comment) is not False
+    ]
--- src/island_code_extractor/reddit.py
+++ src/island_code_extractor/reddit.py
@@ -1,11 +1,15 @@
-# from pprint import pprint
-from typing import List, Tuple
+import sys
+from typing import List, Union
+from pprint import pprint
 
 import pendulum
 import praw
-from praw.models import Submission, Comment
+from praw.models import Submission, Comment, MoreComments
 
 from island_code_extractor import panaetius
+
+
+top_level_comments = []
 
 
 def get_reddit_instance() -> praw.Reddit:
@@ -16,26 +20,89 @@ def get_reddit_instance() -> praw.Reddit:
     )
 
 
-def get_hourly_comments(reddit_instance: praw.Reddit) -> List[Comment]:
-    total_comments = []
+def get_comments_from_thread(
+    delta: int = -1,
+    depth: int = -1,
+    sort: str = "new",
+    relative: Union[int, float, str] = "now",
+) -> List[Comment]:
+
+    reddit_instance = get_reddit_instance()
+    return get_top_level_comments(
+        reddit_instance=reddit_instance,
+        delta=delta,
+        depth=depth,
+        sort=sort,
+        relative=relative,
+    )
+
+
+def get_top_level_comments(
+    reddit_instance: praw.Reddit,
+    delta: int,
+    depth: int,
+    sort: str,
+    relative: Union[int, float, str] = "now",
+) -> List[Comment]:
+
+    if delta < -1:
+        panaetius.logger.error(f"Negative time delta {delta}.")
+        sys.exit(1)
+    # total_comments = []
     submission = reddit_instance.submission(id=panaetius.CONFIG.reddit_thread_id)
-    submission.comment_sort = "new"
-    submission.comments.replace_more(limit=None)
-    for top_level_comment in submission.comments:
-        if _check_hour(top_level_comment):
-            total_comments.append(top_level_comment)
-        else:
-            break
-    return total_comments
+    submission.comment_sort = sort
+    get_more_comments(
+        submission.comments.list(),
+        delta=delta,
+        depth=depth,
+        relative=relative,
+        sort=sort,
+    )
+    # total_comments = [
+    #     i
+    #     for i in top_level_comments
+    #     if ((_check_time(i, delta=delta) or delta != -1) and not i.stickied)
+    # ]
+    return None
+    # return total_comments
 
 
-def _check_hour(comment: Submission):
-    current_time = pendulum.now("Europe/London")
+def get_more_comments(
+    comments_obj: Union[Comment, MoreComments],
+    delta: int,
+    depth: int,
+    sort: str,
+    relative: Union[int, float, str] = "now",
+) -> None:
+
+    for i in comments_obj:
+        if i.depth > depth:
+            continue
+        if isinstance(i, Comment):
+            if i.stickied:
+                continue
+            if _check_time(i, delta=delta, relative=relative):
+                top_level_comments.append(i)
+            else:
+                # if fails the timecheck.
+                if sort in ["new", "old"]:
+                    # if sorted by time, break to stop further api calls.
+                    break
+                # if not sorted by time, keep going
+                continue
+        elif isinstance(i, MoreComments):
+            print(".")
+            get_more_comments(i.comments(), delta=delta, depth=depth, sort=sort)
+
+def _check_time(
+    comment: Comment, delta: int, relative: Union[int, float, str] = "now"
+) -> bool:
+
+    if relative == "now":
+        current_time = pendulum.now("Europe/London")
+    elif relative == "submission":
+        current_time = pendulum.from_timestamp(comment.submission.created_utc)
+    else:
+        current_time = pendulum.from_timestamp(relative)
     comment_time = pendulum.from_timestamp(comment.created_utc)
-    return current_time.diff(comment_time).in_seconds() <= 3600
-
-
-if __name__ == "__main__":
-    reddit = get_reddit_instance()
-    comments = get_hourly_comments(reddit)
-    print(dir(comments[0]))
+    return current_time.diff(comment_time).in_seconds() <= delta
